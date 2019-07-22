7906f63e1409369842b14b915e79097d76dacf8f
Dário <dario.santos@syone.com>
2019-07-22 16:41:21 +0100

    trying to run cli within docker

--- cli.go
+++ cli.go
@@ -10,7 +10,8 @@ import (
 	"context"
 
 	pb "github.com/dariosantos86/shippy-service-consignment/proto/consignment"
-	"google.golang.org/grpc"
+
+	micro "github.com/micro/go-micro"
 )
 
 const (
@@ -29,13 +30,10 @@ func parseFile(file string) (*pb.Consignment, error) {
 }
 
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Did not connect: %v", err)
-	}
-	defer conn.Close()
-	client := pb.NewShippingServiceClient(conn)
+	service := micro.NewService(micro.Name("shippy.cli.consignment"))
+	service.Init()
+
+	client := pb.NewShippingServiceClient("shippy.service.consignment", service.Client())
 
 	// Contact the server and print out its response.
 	file := defaultFilename
